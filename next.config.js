8632bfc95c725f054283c3194cffb8cc9a7ce13b
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-04-16 17:54:00 +0300

    config: iignore type error on build

--- next.config.js
+++ next.config.js
@@ -10,6 +10,13 @@ const nextConfig = (phase, { defaultConfig }) => {
   return {
     ...defaultConfig,
     reactStrictMode: true,
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
       return config
     },
